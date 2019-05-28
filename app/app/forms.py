b9707d7d66d9c3a80fb514ad1f41498519376262
Jared Cara <jcara@luc.edu>
2019-05-27 21:02:14 -0500

    Changes

--- app/app/forms.py
+++ app/app/forms.py
@@ -12,13 +12,13 @@ from wtforms import SelectField, RadioField, SubmitField, DecimalField
 from wtforms.validators import DataRequired, NumberRange
 
 
-##  ExpressedGenesForm class manages initial choices for expressed genes.
+##  InputTypeSelectionForm class manages initial choices consensus/individual.
 #   Input type and submit fields.
-class ExpressedGenesForm(FlaskForm):
+class InputTypeSelectionForm(FlaskForm):
     # Input type is a Radio field, select one or the other.
     # Choices is a list of tuples, [(label, data), (label, data)].
-    input_type = RadioField('Input type', \
-            choices=[('Consensus Tissue', 'Consensus Tissue'), \
+    input_type = RadioField('Input type',\
+            choices=[('Consensus Tissue', 'Consensus Tissue'),\
                 ('Individual Sample', 'Individual Sample')])
     submit = SubmitField('Next')
 
@@ -38,4 +38,29 @@ class ExpressedGenesForm_Consensus(FlaskForm):
             validators=[DataRequired(), NumberRange(min=0, max=1)])
 
     submit = SubmitField('Submit')
+
+
+##  Form_Consensus class manages choices for consensus
+##  tissue comparisons.
+#   Select_tissue, proportion, and submit fields.
+class TissueComparisonForm_Consensus(FlaskForm):
+    # Coerce is used so the selection is dynamic, choices are
+    # queried from the database.
+    select_tissue1 = SelectField('Tissue 1', coerce=int)
+
+    # Proportion is a DecimalField, accepts number between 0,1.
+    proportion = DecimalField('Consensus Proportion',
+            default = 0.95,
+            places=2,
+            validators=[DataRequired(), NumberRange(min=0, max=1)])
     
+    boolean_expression = SelectField('Boolean Expression', 
+            choices=[('AND', 'AND (intersection)'),
+                ('OR', 'OR (union)'),
+                ('XOR', 'XOR (one or the other but not both)'),
+                ('NOR', 'NOR (not present in either)')])
+    
+    select_tissue2 = SelectField('Tissue 2', coerce=int)
+    
+    submit = SubmitField('Submit')
+
--- app/app/routes.py
+++ app/app/routes.py
@@ -18,7 +18,7 @@ from app import app, db
 #   Import database models.
 from app.models import Sample, Gene, GeneQual
 #   Import entry forms.
-from app.forms import ExpressedGenesForm, ExpressedGenesForm_Consensus
+from app.forms import InputTypeSelectionForm, ExpressedGenesForm_Consensus, TissueComparisonForm_Consensus
 
 
 ##  Index route.
@@ -34,8 +34,8 @@ def index():
 #   Displays expressed genes options, consensus tissue and ...
 @app.route('/ExpressedGenes', methods=['GET', 'POST'])
 def expressedgenes():
-    # Loads ExpressedGenesForm.
-    form = ExpressedGenesForm()
+    # Loads InputTypeSelectionForm.
+    form = InputTypeSelectionForm()
     
     # If form entry is valid, then submit form.
     if form.validate_on_submit():
@@ -93,10 +93,9 @@ def expressedgenes_consensustissue():
         '''
         # Initialize results to empty list.
         results = []
-        
         # Proportion for consensus calculation from entry field.
-        calc = float(db.session.query(Sample)\
-                .filter(Sample.tissue==choices[form.select_tissue.data][1])\
+        calc = float(db.session.query(Sample)
+                .filter(Sample.tissue==choices[form.select_tissue.data][1])
                 .count() * form.proportion.data)
         
         # Loops GeneQual database for all genes.
@@ -109,19 +108,187 @@ def expressedgenes_consensustissue():
             #       Append gene to results.
             if db.session.query(Sample)\
                     .outerjoin(Sample.genes)\
-                    .filter(Sample.tissue==choices[form.select_tissue.data][1],\
-                        Gene.gene_id==each.gene_id,\
+                    .filter(Sample.tissue==choices[form.select_tissue.data][1],
+                        Gene.gene_id==each.gene_id,
                         Gene.barcode==True)\
                     .count() >= calc:
                         results.append(each)
 
         # Render results.
-        return render_template('expressedgenes_consensustissue_results.html',\
-                title='Results',\
+        return render_template('results.html',
+                title='Results',
                 results=results)
     
     # Render consensus tissue selection page.
-    return render_template('expressedgenes_consensustissue.html',\
-            title='Consensus Tissue',\
+    return render_template('expressedgenes_consensustissue.html',
+            title='Expressed Genes',
             form=form)
 
+
+##  ExpressedGenes route.
+#   Displays expressed genes options, consensus tissue and ...
+@app.route('/TissueComparison', methods=['GET', 'POST'])
+def tissuecomparison():
+    # Loads InputTypeSelectionForm.
+    form = InputTypeSelectionForm()
+
+    # If form entry is valid, then submit form.
+    if form.validate_on_submit():
+        # Operator to select consensus tissue or ...
+        if form.input_type.data == 'Consensus Tissue':
+            # Redirect to consensus tissue.
+            return redirect(url_for('tissuecomparison_consensustissue'))
+        else:
+            flash(form.input_type.data)
+            return redirect(url_for('index'))
+
+    # Render expressedgenes page.
+    return render_template('tissuecomparison.html',\
+            title='Tissue Comparison',\
+            form=form)
+
+
+##  Expressedgenes_consensustissue route.
+#   Displays consensus tissue options, displays results.
+@app.route('/TissueComparison/ConsensusTissue', methods=['GET', 'POST'])
+def tissuecomparison_consensustissue():
+    # Query all Samples to find unique tissues.
+    # This is not optimal, working on a permanent, efficient query.
+    all_samples = Sample.query.all()
+    unique_tissues = []
+
+    # Loops each sample to add unique to all_tissues.
+    for each in all_samples:
+        if each.tissue not in unique_tissues:
+            unique_tissues.append(each.tissue)
+
+    # Sets choices to unique tissues.
+    choices = [(i, unique_tissues[i]) for i in range(len(unique_tissues))]
+
+    # Initializes Consensus tissue form.
+    form = TissueComparisonForm_Consensus()
+    # Sets choices to unique tissues.
+    form.select_tissue1.choices = choices
+    form.select_tissue2.choices = choices
+
+    # If form is valid, submit form.
+    if form.validate_on_submit():
+        # Initialize results to empty list.
+        results = []
+        
+        start = timeit.default_timer()
+        # Proportion for consensus calculation from entry field.
+        calc1 = float(db.session.query(Sample)\
+                .filter(Sample.tissue==choices[form.select_tissue1.data][1])\
+                .count() * form.proportion.data)
+        calc2 = float(db.session.query(Sample)\
+                .filter(Sample.tissue==choices[form.select_tissue2.data][1])\
+                .count() * form.proportion.data)
+
+        if form.boolean_expression.data == 'AND':
+            # Loops GeneQual database for all genes.
+            for each in db.session.query(GeneQual).all():
+                # Query Sample, Gene database.
+                # Query Sample and Gene reference,
+                #       First filter: tissue type.
+                #       Second filter: each gene id in loop and barcode is True.
+                # If the count of query is >= proportion,
+                #       Append gene to results.
+                if db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue1.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc1\
+                    and db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue2.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc2:
+                            results.append(each)
+
+        elif form.boolean_expression.data == 'OR':
+            # Loops GeneQual database for all genes.
+            for each in db.session.query(GeneQual).all():
+                # Query Sample, Gene database.
+                # Query Sample and Gene reference,
+                #       First filter: tissue type.
+                #       Second filter: each gene id in loop and barcode is True.
+                # If the count of query is >= proportion,
+                #       Append gene to results.
+                if db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue1.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc1\
+                    or db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue2.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc2:
+                            results.append(each)
+        elif form.boolean_expression.data == 'XOR':
+            # Loops GeneQual database for all genes.
+            for each in db.session.query(GeneQual).all():
+                # Query Sample, Gene database.
+                # Query Sample and Gene reference,
+                #       First filter: tissue type.
+                #       Second filter: each gene id in loop and barcode is True.
+                # If the count of query is >= proportion,
+                #       Append gene to results.
+                if (db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue1.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc1)\
+                    ^ (db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue2.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() >= calc2):
+                            results.append(each)
+        elif form.boolean_expression.data == 'NOR':
+            # Loops GeneQual database for all genes.
+            for each in db.session.query(GeneQual).all():
+                # Query Sample, Gene database.
+                # Query Sample and Gene reference,
+                #       First filter: tissue type.
+                #       Second filter: each gene id in loop and barcode is True.
+                # If the count of query is >= proportion,
+                #       Append gene to results.
+                if (db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue1.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() <= calc1)\
+                    and (db.session.query(Sample)\
+                        .outerjoin(Sample.genes)\
+                        .filter(Sample.tissue==choices[form.select_tissue2.data][1],\
+                            Gene.gene_id==each.gene_id,\
+                            Gene.barcode==True)\
+                        .count() <= calc2):
+                            results.append(each)
+        stop = timeit.default_timer()
+        print('Time: ', stop-start)
+        # Render results.
+        return render_template('results.html',\
+                title='Results',\
+                results=results)
+
+    # Render consensus tissue selection page.
+    return render_template('tissuecomparison_consensustissue.html',\
+            title='Expressed Genes',\
+            form=form)
+
+
+'''
+@app.route('/GeneCatalog', methods=['GET', 'POST'])
+def genecatalog():
+    
+'''
